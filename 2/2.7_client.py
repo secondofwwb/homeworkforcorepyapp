52a917981a8eb0f36b0a8e88f6432acca5532bdd
second <wuwenbosecond@163.com>
2018-01-18 20:39:05 +0800

    全双工未完成

--- 2/2.7_client.py
+++ 2/2.7_client.py
@@ -1,13 +1,14 @@
 # Echo client program
 import socket
 
-HOST = 'localhost'
-PORT = 50007
-socket_c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_c.connect((HOST, PORT))
 
-while True:
-    input_data = input('>')
-    socket_c.sendall(input_data.encode())
-    data = socket_c.recv(1024)
-    print('Received', data.decode())
+HOST = 'localhost'
+PORT_client = 50007
+
+
+
+if __name__ == '__main__':
+    socket_c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        send_data = input('>')
+        socket_c.sendto(send_data.encode(), (HOST, PORT_client))
--- 2/2.7_server.py
+++ 2/2.7_server.py
@@ -1,21 +1,40 @@
 import socket
-
+import hashlib
+'''
+程序未完成
+'''
 
 HOST = 'localhost'
-PORT = 50007
-socket_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT_server = 50007
 
-socket_s.bind((HOST, PORT))
-socket_s.listen(1)
+list_flag = {}
 
-while True:
-    print('-----server start------')
-    conn, addr = socket_s.accept()
-    print('accept connet from:', addr)
-    print('-----wait message------')
+def save_client(data,addr,list_flag):
+    host = addr[1]
+    port = addr[2]
+    string_flag = str(host)+str(port)
+    flag = hashlib.md5(string_flag.encode())
+    # username = data.decode()
+    dict = {'host':host, 'port':port, 'flag':flag}
+    list_flag[flag] = dict
+    print('new user online')
 
+
+if __name__ == '__main__':
+    print('------start server-----')
+    socket_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_s.bind((HOST, PORT_server))
+    print('---waiting for message---')
     while True:
-        rec_data = conn.recv(1024)
-        print('Received', rec_data.decode())
-        send_data = input('>')
-        conn.sendall(send_data.encode())
+        data, addr = socket_s.recvfrom(1024)
+        if data.decode() == 'on1line':
+            save_client(data,addr,list_flag)
+        if data.decode() == 'touser+username':
+
+
+        socket_s.sendto(data2, addr2)
+
+
+
+
+
